fc8159afc7a84e56c1b258dab5d96696971672df
DudisTrindade <eduarda.trindade@escolar.ifrn.edu.br>
2025-07-16 16:24:24 -0300

    Cena nova 2

--- 2bimestre POO/Assets/Cod/Load Scene.cs
+++ 2bimestre POO/Assets/Cod/Load Scene.cs
@@ -1,2 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+public class LoadScene : MonoBehaviour
+{
+  public string sceneName;
+
+  public void Load()
+  {
+    SceneManager.LoadScene(sceneName);
+  }
+  
+}
---
+++ 2bimestre POO/Assets/Cod/TrocarTexto.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TrocarTexto : MonoBehaviour
+{
+   public TextMeshProUGUI texto;
+   public TMP_InputField inputField;
+   
+   public void Trocar()
+   
+}
