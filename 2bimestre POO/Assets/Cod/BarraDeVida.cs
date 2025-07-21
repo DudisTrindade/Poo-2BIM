ab6a35f29dd7895a2fa38bfd8053797ff710e1ca
DudisTrindade <eduarda.trindade@escolar.ifrn.edu.br>
2025-07-21 14:28:54 -0300

    Novo botão

---
+++ 2bimestre POO/Assets/Cod/BarraDeVida.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class BarraDeVida : MonoBehaviour
+{
+    public Jogador jogador;
+    [SerializeField] 
+    private int vidasRestantes = 0
+   [SerializeField]
+
+    private int energiaRestante = 0
+
+    public Slider slider_vidasRestantes;
+    public Slider slider_energiaRestantes;
+
+    void Start()
+    {
+        if ()
+    }
+  
+}
